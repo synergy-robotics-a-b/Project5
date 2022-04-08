0656df39e796aeb57ace76a458a81343d1f5b3ba
vedek <52510362+vedek@users.noreply.github.com>
2022-04-08 18:07:02 -0500

    Update utilities.cpp

--- utilities.cpp
+++ utilities.cpp
@@ -115,3 +115,173 @@ struct node* deleteNode(struct node* root, int key)
 }
 
 //--------------------------------------
+/C) Report is the tree contains a specific piece of data :
+
+#include <iostream>
+using namespace std;
+
+// Binary tree node
+struct Node {
+    int data;
+    struct Node* left, * right;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+// Function to traverse the tree in preorder
+// and check if the given node exists in it
+bool ifNodeExists(struct Node* node, int key)
+{
+    if (node == NULL)
+        return false;
+
+    if (node->data == key)
+        return true;
+
+    /* then recur on left subtree */
+    bool res1 = ifNodeExists(node->left, key);
+    // node found, no need to look further
+    if (res1) return true;
+
+    /* node is not found in left,
+    so recur on right subtree */
+    bool res2 = ifNodeExists(node->right, key);
+
+    return res2;
+}
+
+//---------------------------------------------
+
+//Dand E) MAXIMUM AND SMALLEST NODE IN THE BINARY TREE :
+
+
+// A tree node
+class Node {
+public:
+    int data;
+    Node* left, * right;
+
+    /* Constructor that allocates a new
+    node with the given data and NULL
+    left and right pointers. */
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+// Returns maximum value in a given
+// Binary Tree
+int findMax(Node* root)
+{
+    // Base case
+    if (root == NULL)
+        return INT_MIN;
+
+    // Return maximum of 3 values:
+    // 1) Root's data 2) Max in Left Subtree
+    // 3) Max in right subtree
+    int res = root->data;
+    int lres = findMax(root->left);
+    int rres = findMax(root->right);
+    if (lres > res)
+        res = lres;
+    if (rres > res)
+        res = rres;
+    return res;
+}
+
+int findMin(Node* root)
+{
+    //code
+    if (root == NULL)
+    {
+        return INT_MAX;
+    }
+    int res = root->data;
+    int left = findMin(root->left);
+    int right = findMin(root->right);
+    if (left < res)
+    {
+        res = left;
+    }
+    if (right < res)
+    {
+        res = right;
+    }
+    return res;
+}
+
+
+/*  Tree node structure */
+struct Node
+{
+    int key;
+    struct Node* left, * right;
+};
+
+/* Helper function that allocates a new node with the
+   given key and NULL left and right pointer. */
+struct Node* newNode(char k)
+{
+    struct Node* node = new  Node;
+    node->key = k;
+    node->right = node->left = NULL;
+    return node;
+}
+bool isFullTree(struct Node* root)
+{
+    // If empty tree
+    if (root == NULL)
+        return true;
+
+    // If leaf node
+    if (root->left == NULL && root->right == NULL)
+        return true;
+
+    // If both left and right are not NULL, and left & right subtrees
+    // are full
+    if ((root->left) && (root->right))
+        return (isFullTree(root->left) && isFullTree(root->right));
+
+    // We reach here when none of the above if conditions work
+    return false;
+}
+
+//------------------------------
+
+//F) print all the binary tree :
+
+
+struct Node
+{
+    int data;
+    struct Node* left, * right;
+};
+
+Node* newNode(int data)
+{
+    Node* temp = new Node;
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// Traverses given tree in Inorder fashion and
+// prints all nodes that have both children as
+// non-empty.
+void findFullNode(Node* root)
+{
+    if (root != NULL)
+    {
+        findFullNode(root->left);
+        if (root->left != NULL && root->right != NULL)
+            cout << root->data << " ";
+        findFullNode(root->right);
+    }
+}
