0ced409ce5014dade304ba0ebb95742fba46389c
vedek <52510362+vedek@users.noreply.github.com>
2022-04-08 17:58:52 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+#include BT.h
+
+int main()
+{
+    Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->right->left = newNode(5);
+    root->right->right = newNode(6);
+    root->right->left->right = newNode(7);
+    root->right->right->right = newNode(8);
+    root->right->left->right->left = newNode(9);
+    findFullNode(root);
+    return 0;
+}
